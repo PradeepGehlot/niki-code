d9029c22d203a3ec8c6f22615bbf6156273d0cde
Pradeep Gehlot <psgehlot7988@gmail.com>
2017-10-18 21:36:05 +0530

    niki code

---
+++ niki.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+void multiply(long long F[2][2], long long M[2][2]);
+
+void power(long long F[2][2], long long n);
+long long fib(long long n)
+{
+  long long F[2][2] = {{1,1},{1,0}};
+  if (n == 0)
+    return 0;
+  power(F, n-1);
+  return F[0][0]%1000000007;
+}
+void power(long long F[2][2], long long n)
+{
+  if( n == 0 || n == 1)
+      return;
+  long long M[2][2] = {{1,1},{1,0}};
+
+  power(F, n/2);
+  multiply(F, F);
+
+  if (n%2 != 0)
+     multiply(F, M);
+}
+
+void multiply(long long F[2][2], long long M[2][2])
+{
+  long long x =  (F[0][0]*M[0][0])%1000000007 + (F[0][1]*M[1][0])%1000000007;
+  long long y =  (F[0][0]*M[0][1])%1000000007 + (F[0][1]*M[1][1])%1000000007;
+  long long z =  (F[1][0]*M[0][0])%1000000007 + (F[1][1]*M[1][0])%1000000007;
+  long long w =  (F[1][0]*M[0][1])%1000000007 + (F[1][1]*M[1][1])%1000000007;
+
+  F[0][0] = x;
+  F[0][1] = y;
+  F[1][0] = z;
+  F[1][1] = w;
+}
+int main()
+{
+
+ long long  t;
+ cin>>t;
+ while(t--){
+     long long n;
+     cin>>n;
+      cout<<fib(n+2)<<endl;
+ }
+
+  return 0;
+}
